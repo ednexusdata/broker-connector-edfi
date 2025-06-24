c5e51324b1458de11d7a99a1d88b5de312bcc7be
Makoa Jacobsen <makoa@makoajacobsen.com>
2025-06-24 03:46:28 +0000

    Add edfi version to config

--- src/Configuration/Connection.cs
+++ src/Configuration/Connection.cs
@@ -1,18 +1,25 @@
 using System.ComponentModel.DataAnnotations;
 using EdNexusData.Broker.Common.Configuration;
+using EdNexusData.Broker.Common.Lookup;
+using EdNexusData.Broker.Connector.EdFiAlliance.EdFi.ConfigurationLookups;
 
 namespace EdNexusData.Broker.Connector.EdFiAlliance.EdFi.Configuration;
 
 [DisplayName("Connection")]
 public class Connection : IConfiguration
 {
+    [Lookup(typeof(EdFiVersionConfigurationLookup))]
+    [DisplayName("Version")]
+    [Description("The version of the Ed-Fi instance.")]
+    public string EdFiVersion { get; set; } = default!;
+
     [DataType(DataType.Url)]
-    [DisplayName("Ed-Fi API OAuth URL")]
+    [DisplayName("API OAuth URL")]
     [Description("The URL of the OAuth endpoint of the Ed-Fi API instance to connect to.")]
     public string EdFiApiOAuthUrl { get; set; } = default!;
 
     [DataType(DataType.Url)]
-    [DisplayName("Ed-Fi API URL")]
+    [DisplayName("API URL")]
     [Description("The URL of the Ed-Fi API instance to connect to.")]
     public string EdFiApiUrl { get; set; } = default!;
 
